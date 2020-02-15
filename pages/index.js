6b5c42506431e37254b322eba7f89d93f1c15c04
castaneai <castaneai@castaneai.net>
2020-02-15 21:23:39 +0900

    hide pinned page

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,16 @@ const Index = props => (
   <div>
     <h1>{process.env.projectName}</h1>
     <ul>
-      {props.posts.map(post => (
-        <li key={post.id}>
-          <span>{moment.unix(post.created).format("YYYY/MM/DD")}</span>
-          <Link href="/[title]" as={`/${post.title}`}>
-            <a>{post.title}</a>
-          </Link>
-        </li>
-      ))}
+      {props.posts
+        .filter(post => post.pin === 0)
+        .map(post => (
+          <li key={post.id}>
+            <span>{moment.unix(post.created).format("YYYY/MM/DD")}</span>
+            <Link href="/[title]" as={`/${post.title}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
     </ul>
   </div>
 );
